20599b0cb86fc60323b5e4f43f24dddfe8dde848
MikiP98 <pokora.mikolaj@gmail.com>
2025-12-17 22:56:27 +0100

    Implemented the config and database checks

--- logistics/main.py
+++ logistics/main.py
@@ -1,44 +1,102 @@
-import os
+import dataclasses
+import sys
+from dataclasses import dataclass
 from pathlib import Path
 
-from logistics.io_utils import ask_for_bool, warn
+import toml
+from platformdirs import user_config_dir
+
 from logistics.database.setup import try_setup_new_database
+from logistics.io_utils import ask_for_bool, ask_for_string, warn
 from logistics.pipeline_loops.manager import start_pipeline_loops
 
 
-def main() -> None:
-    # Check if config exists, if not, create it
-    # Read config and check for database, if it does not exist, create it
+@dataclass(frozen=True, slots=True)
+class Config:
+    database_location: str
+    database_name: str
 
-    # TODO: Consider moving config and database checks to separate functions
+    @property
+    def database_path(self):
+        return Path(self.database_location, self.database_name)
 
-    config_location = "./"
-    # TODO: Change the config location to the correct Windows and Linux path
-    config_filename = "config.toml"
 
-    if not os.path.exists(config_location + config_filename):
-        # create_config = ask_for_bool("No config file has been found, do you want to setup a new one now?")
-        # TODO
-        ...
+def save_config(config: Config, config_path: Path):
+    with config_path.open("w") as f:
+        f.write(toml.dumps(dataclasses.asdict(config)))
 
-    # TODO: Read the config from the database
-    database_location = "./"
-    database_filename = "humble_logistics.sqlite"
 
-    if not os.path.exists(database_location + database_filename):
+def get_config(config_dir, config_file) -> Config | None:
+    if not config_file.exists():
+        create_config = ask_for_bool("No config file has been found, do you want to setup a new one now?")
+        if create_config:
+            database_location = ask_for_string(
+                "Provide the path database should be in.\n"
+                "Leave empty for current directory\n"
+                "Provide '%config%' for config file location"
+            )
+            if len(database_location) == 0:
+                database_location = "./"
+            else:
+                database_location = database_location.replace("%config%", str(config_dir))
+
+            database_filename = ask_for_string(
+                "Provide the database name.\n"
+                "Leave empty for current directory\n"
+                "Provide '%config%' for config file location"
+            )
+            if len(database_filename) == 0:
+                database_filename = "humble_logistics.sqlite"
+
+            config = Config(database_location=database_location, database_name=database_filename)
+
+            config_dir.mkdir(parents=True, exist_ok=True)
+            save_config(config, config_file)
+        else:
+            return None
+    else:
+        with config_file.open() as f:
+            config = Config(**toml.load(f))
+    return config
+
+
+def check_for_database(config: Config, config_path: Path) -> bool:
+    if not config.database_path.exists():
         create_db = ask_for_bool("No database has been found, do you want to setup a new one now?")
         if create_db:
-            try_setup_new_database(database_location, database_filename)
-            # TODO: Return db_path and db_filename back from the setup function
-            #  in case the location changed and config file has to be updated
-            # TODO: Handle config setup returning False
+            success, (database_location, database_filename) = try_setup_new_database(
+                config.database_location, config.database_name
+            )
+            if not success:
+                warn("Database could not be created, exiting")
+                return False
+            if config.database_location != database_location or config.database_name != database_filename:
+                config = Config(database_location=database_location, database_name=database_filename)
+                with config_path.open("w") as f:
+                    f.write(toml.dumps(dataclasses.asdict(config)))
         else:
             warn("No database to connect to, existing")
-            return
+            return False
+    return True
+
+
+def main() -> int:
+    config_dir = Path(user_config_dir(appname="logistics", appauthor="pipr", roaming=True))
+    config_file = config_dir / "config.toml"
+
+    config = get_config(config_dir, config_file)
+    if config is None:
+        warn("No config file exists, exiting")
+        return 1
+
+    if not check_for_database(config, config_file):
+        return 1
 
     # Start the pipeline loops
-    start_pipeline_loops(Path(database_location) / database_filename)
+    start_pipeline_loops(config.database_path)
+
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
