7ab5737f658cf8637756dc865e431fe1105681a6
mikip98 <pokora.mikolaj@gmail.com>
2025-12-17 13:33:13 +0100

    Accept db path and virtual clocks as inputs

--- logistics/pipeline_loops/console_loop.py
+++ logistics/pipeline_loops/console_loop.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 from enum import Enum
+from pathlib import Path
+
+from logistics.pipeline_loops.virtual_clock import VirtualClock
 
 
 class Commands(Enum):
     DELETE_CONFIG = None
 
 
-def run_console_loop():
+def run_console_loop(db_path: Path, clock: VirtualClock):
     ...
