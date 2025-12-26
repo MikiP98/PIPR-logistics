72a0b83ec00767f598aa8e0f499954508d473e6f
MikiP98 <pokora.mikolaj@gmail.com>
2025-12-26 21:01:48 +0100

    Cleaned up the event loop code

--- logistics/pipeline_loops/event_loop.py
+++ logistics/pipeline_loops/event_loop.py
@@ -1,19 +1,19 @@
-import math
-import sqlite3
+# import math
 import time
 from pathlib import Path
 
+# from database.database import Database
 from logistics.pipeline_loops.virtual_clock import VirtualClock
 
 
 def run_event_loop(db_path: Path, clock: VirtualClock) -> None:
-    conn = sqlite3.connect(db_path, timeout=10)
-    cursor = conn.cursor()
-
-    # We track the last virtual minute we successfully processed
-    # We round down to the nearest minute to align cleanly
-    last_processed_virtual = math.floor(clock.get_time() / 60) * 60
+    # database = Database(db_path)
+    # Track the last virtual minute we successfully processed
+    # Round down to the nearest minute to align cleanly
+    # last_processed_virtual = math.floor(clock.get_time() / 60) * 60
 
     while True:
-        time.sleep(60 / clock.scale)
+        # TODO: Implement logic
+
+        time.sleep(60 / clock.get_scale())
         # TODO: Sync to the end of a minute
